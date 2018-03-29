c5b60bf42761f7eab4d289860002a15286c52c7e
Jeremy <jeremy.duran@gmail.com>
2018-03-29 12:08:20 +0100

    added menu

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -1,5 +1,9 @@
 import React from 'react';
-//import {Tabs, Tab} from 'material-ui/Tabs';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreHorizIcon from 'material-ui/svg-icons/navigation/more-horiz';
+
 //import SwipeableViews from 'react-swipeable-views';
 import './profile.css';
 import ScrollButton from '../scroll-button';
@@ -15,12 +19,68 @@ export default class ProfileTabs extends React.Component {
     }
 
     componentDidMount() {
-        console.log(this.state.id);
+        //console.log(this.state.id);
     }
+    //function(event: object, child: object) => void
+    handleMenuItemClick = event => {
+        console.log(event.target.textContent);
+        //*
+        switch (event.target.textContent) {
+            case 'Refresh':
+                window.location.reload();
+                break;
+
+            default:
+                //console.log();
+                break;
+        }
+        // */
+    };
     render() {
         return (
             <div>
                 <div className="container red">
+                    <div
+                        style={{
+                            right: '5%',
+                            top: '5px',
+                            zIndex: 2,
+                            position: 'absolute'
+                        }}
+                    >
+                        <IconMenu
+                            animated={false}
+                            iconButtonElement={
+                                <IconButton>
+                                    <MoreHorizIcon color={'#585462'} />
+                                </IconButton>
+                            }
+                            anchorOrigin={{
+                                horizontal: 'left',
+                                vertical: 'top'
+                            }}
+                            targetOrigin={{
+                                horizontal: 'left',
+                                vertical: 'top'
+                            }}
+                        >
+                            <MenuItem
+                                onClick={this.handleMenuItemClick}
+                                value={1}
+                                primaryText="Refresh"
+                            />
+                            <MenuItem
+                                onClick={this.handleMenuItemClick}
+                                value={2}
+                                primaryText="Send feedback"
+                            />
+                            <MenuItem
+                                onClick={this.handleMenuItemClick}
+                                value={3}
+                                primaryText="Settings"
+                            />
+                        </IconMenu>{' '}
+                    </div>
                     <div className="item left ">
                         <div className="leftContent">
                             <h1 className="username">
