7f6cf9d7d69e93b95142dd30c9d593acbd1898ea
Jeremy <jeremy.duran@gmail.com>
2018-04-20 14:43:49 +0100

    link team page to team api

--- src/profile-extended-teams/profile-extended-team.js
+++ src/profile-extended-teams/profile-extended-team.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './profile-extended-team.css';
+import { userId } from '../profile/profile';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import Avatar from 'material-ui/Avatar';
+import CircularProgress from 'material-ui/CircularProgress';
 
-const teams = [
-    { id: 1, teamName: 'Team1', teamDivison: 'Silver', league: '2v2' },
-    { id: 2, teamName: 'Team2', teamDivison: 'Bronze', league: '3v3' },
-    { id: 3, teamName: 'Team3', teamDivison: 'Gold', league: '2v2' },
-    { id: 4, teamName: 'Team4', teamDivison: 'Platinum', league: '2v2' }
-];
 const styles = {
     FirstRowTitle: {
         fontFamily: 'Roboto Medium',
@@ -51,12 +47,25 @@ const styles = {
     }
 };
 
+function Loader() {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <CircularProgress size={60} thickness={7} />
+        </div>
+    );
+}
+
 function Teams(props) {
     const content = props.teams.map(team => (
         <div key={team.id} className="team-card">
-            <p className="teamHeader">TEAM {team.league}</p>
+            <p className="teamHeader">
+                {' '}
+                {team.attributes.name === '' ? 'TBD' : team.attributes.name}
+            </p>
             <Avatar
-                src={require('../images/Tournament_Winner_01.png')}
+                src={require('../images/' +
+                    (team.attributes.stats.league + 1) +
+                    '.png')}
                 className="inline"
                 style={styles.avatar}
                 size={30}
@@ -71,7 +80,9 @@ function Teams(props) {
                 }}
             >
                 <Avatar
-                    src={require('../images/6.png')}
+                    src={require('../images/' +
+                        (team.attributes.stats.league + 1) +
+                        '.png')}
                     backgroundColor={'none'}
                     style={{
                         marginTop: 'auto',
@@ -104,10 +115,27 @@ function Teams(props) {
 }
 
 export default class ExtendedProfileTeam extends React.Component {
-    state = {
-        value: 1
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 7,
+            userId: props.userId,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        getTeam(this);
+    }
+    //*
+
+    handleChange = (event, index, value) => {
+        this.setState({ value: value, isLoaded: false }, () => {
+            //console.log("state value" + this.state.value);
+            getTeam(this);
+        });
     };
-    handleChange = (event, index, value) => this.setState({ value });
+
     render() {
         return (
             <div>
@@ -137,11 +165,8 @@ export default class ExtendedProfileTeam extends React.Component {
                                     fontSize: '0.8em'
                                 }}
                             >
-                                <MenuItem value={1} primaryText="Season 1" />
-                                <MenuItem value={2} primaryText="Season -1" />
-                                <MenuItem value={3} primaryText="Season -2" />
-                                <MenuItem value={4} primaryText="Season -3" />
-                                <MenuItem value={5} primaryText="Season -4" />
+                                <MenuItem value={7} primaryText="Season 1" />
+                                <MenuItem value={6} primaryText="Pre-Season" />
                             </SelectField>
                         </div>
                         <div className="refresh">
@@ -154,8 +179,50 @@ export default class ExtendedProfileTeam extends React.Component {
                     </div>
                 </div>
 
-                <Teams classname="teamArea" teams={teams} />
+                <div>
+                    {this.state.isLoaded ? (
+                        <Teams classname="teamArea" teams={this.state.items} />
+                    ) : (
+                        <Loader />
+                    )}
+                </div>
             </div>
         );
     }
+}
+function getTeam(ref) {
+    fetch(
+        'https://api.dc01.gamelockerapp.com/shards/global/teams?tag[season]=' +
+            ref.state.value +
+            '&tag[playerIds]=' +
+            ref.state.userId,
+        {
+            method: 'GET',
+            headers: {
+                Authorization:
+                    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI3MzRiOTU5MC1mZDM4LTAxMzUtNmNhZC0wYTU4NjQ2MGE2NGYiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTE5NjU5MjY1LCJwdWIiOiJzdHVubG9jay1zdHVkaW9zIiwidGl0bGUiOiJiYXR0bGVyaXRlIiwiYXBwIjoiYmF0dGxldS0zN2RhZGFlZC1hY2FkLTQ3MjctODZiMS02ZTJlOGM5NGFlOWQiLCJzY29wZSI6ImNvbW11bml0eSIsImxpbWl0IjoxMH0.jf1RKIU35VN05oIp4031YxOYyLHSDlvQ0fefnoCCBNQ',
+                Accept: 'application/json'
+            }
+        }
+    )
+        .then(res => res.json())
+        .then(
+            result => {
+                ref.setState({
+                    isLoaded: true,
+                    items: result.data
+                });
+                console.log(ref.state.items);
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            error => {
+                ref.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        );
+    //*/
 }
